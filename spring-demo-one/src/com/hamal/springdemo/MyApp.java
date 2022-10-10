6430e446211cc876def30ea0bc81d74344e3c9b6
PradipH31 <pradip.hamal31@gmailcom>
2022-10-09 23:52:37 -0500

    First implementation of a class

---
+++ spring-demo-one/src/com/hamal/springdemo/MyApp.java
@@ -0,0 +1,13 @@
+package com.hamal.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// create object
+		BaseballCoach theCoach = new BaseballCoach();
+		System.out.println(theCoach.getDailyWorkout());
+		// use object
+	}
+
+}
