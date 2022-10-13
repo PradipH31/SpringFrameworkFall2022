72e0b2ccef59b1d4a7c54d947d03240b49e29d56
PradipH31 <pradip.hamal31@gmail.com>
2022-10-12 22:13:33 -0500

    Configured dependency injection in class and context

--- spring-demo-one/src/com/hamal/springdemo/TrackCoach.java
+++ spring-demo-one/src/com/hamal/springdemo/TrackCoach.java
@@ -2,6 +2,14 @@ package com.hamal.springdemo;
 
 public class TrackCoach implements Coach {
 
+	// define a private field for the dependency
+	private FortuneService fortuneService;
+
+	// define a constructor for dependency injection
+	public TrackCoach(FortuneService theFortuneService) {
+			fortuneService = theFortuneService;
+		}
+
 	@Override
 	public String getDailyWorkout() {
 		return "Return a hard 5k";
@@ -9,8 +17,7 @@ public class TrackCoach implements Coach {
 
 	@Override
 	public String getDailyFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		return fortuneService.getFortune();
 	}
 
 }
