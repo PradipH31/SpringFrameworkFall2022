62155ef60d8a7a7a6e9ae7bd8ac1794e21876580
PradipH31 <pradip.hamal31@gmail.com>
2022-10-12 22:08:16 -0500

    Defined dependencies for Coach beans

---
+++ spring-demo-one/src/com/hamal/springdemo/FortuneService.java
@@ -0,0 +1,5 @@
+package com.hamal.springdemo;
+
+public interface FortuneService {
+	public String getFortune();
+}
--- spring-demo-one/src/com/hamal/springdemo/TrackCoach.java
+++ spring-demo-one/src/com/hamal/springdemo/TrackCoach.java
@@ -7,4 +7,10 @@ public class TrackCoach implements Coach {
 		return "Return a hard 5k";
 	}
 
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
