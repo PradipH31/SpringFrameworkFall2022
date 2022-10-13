9a55c4ae4459faf4e761cff22aec1121b173d481
PradipH31 <pradip.hamal31@gmail.com>
2022-10-13 09:21:06 -0500

    Set up setter injection

--- spring-demo-one/src/com/hamal/springdemo/CricketCoach.java
+++ spring-demo-one/src/com/hamal/springdemo/CricketCoach.java
@@ -15,6 +15,10 @@ public class CricketCoach implements Coach {
 		return "Practice fast bowling for 15 minutes";
 	}
 
+	public void setFortuneService(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+
 	@Override
 	public String getDailyFortune() {
 		return fortuneService.getFortune();
